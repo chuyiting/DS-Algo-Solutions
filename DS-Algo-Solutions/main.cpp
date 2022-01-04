5e2b4e5f20c6587e8ce11451b3e94df2cf77bdb9
Eddy <eddy.chi.chu@gmail.com>
2022-01-04 18:10:11 +0800

    Initial Commit

---
+++ DS-Algo-Solutions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS-Algo-Solutions
+//
+//  Created by Eddy Chu on 2022/1/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
