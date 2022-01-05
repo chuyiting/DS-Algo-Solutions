e5649a24c5e23b2eb07cc6d631bdceb720a31177
Eddy <eddy.chi.chu@gmail.com>
2022-01-05 22:55:17 +0800

    Finish LeetCode Problem 153

---
+++ DS-Algo-Solutions/Week1/153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,34 @@
+//
+//  153-find-minimum-in-rotated-sorted-array.cpp
+//  DS-Algo-Solutions
+//
+//  Created by Eddy Chu on 2022/1/5.
+//
+
+#include <stdio.h>
+
+class Solution {
+public:
+    int findMin(std::vector<int>& nums) {
+        int i = 0, j = nums.size() - 1, mid = (i + j) / 2;
+       
+        while (j > i) {
+            if (j - i + 1 == 2) {
+                return std::min(nums[i], nums[j]);
+            }
+            
+            mid = (i + j) / 2;
+            if (nums[j] > nums[i]) {
+                return nums[i];
+            }
+            
+            if (nums[mid] > nums[i]) {
+                i = mid;
+            } else {
+                j = mid;
+            }
+        }
+        
+        return nums[i];
+    }
+};
