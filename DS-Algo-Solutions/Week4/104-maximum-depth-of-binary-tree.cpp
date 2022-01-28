93d9e5af385284e7cdc66c09e1a1fc59bba07abb
Eddy <eddy.chi.chu@gmail.com>
2022-01-29 02:56:28 +0800

    Finish leetcode problem 104

---
+++ DS-Algo-Solutions/Week4/104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,28 @@
+//
+//  104-maximum-depth-of-binary-tree.cpp
+//  DS-Algo-Solutions
+//
+//  Created by Eddy Chu on 2022/1/29.
+//
+
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 1;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
